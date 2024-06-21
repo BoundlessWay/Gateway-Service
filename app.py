d4a21965c6044e07d8769f0e48d6a0e42751490b
BoundlessWay <70361753+BoundlessWay@users.noreply.github.com>
2024-06-21 19:08:49 +0700

    add tag Fast API

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 from routes import account, event
 
-app = FastAPI()
+app = FastAPI(docs_url="/")
 
 # Include routers from account and event modules
-app.include_router(account.router, prefix="/account")
-app.include_router(event.router, prefix="/event")
+app.include_router(account.router, prefix="/account", tags=["Account"])
+app.include_router(event.router, prefix="/event", tags=["Event"])
 
 if __name__ == "__main__":
     import uvicorn
