7af7352ce4a42543b7d20a97bb8a5e549cac4562
BoundlessWay <70361753+BoundlessWay@users.noreply.github.com>
2024-06-21 19:20:25 +0700

    delete __name__ in app.py

--- app.py
+++ app.py
@@ -7,8 +7,3 @@ app = FastAPI(docs_url="/")
 app.include_router(account.router, prefix="/account", tags=["Account"])
 app.include_router(event.router, prefix="/event", tags=["Event"])
 
-if __name__ == "__main__":
-    import uvicorn
-
-    # Run FastAPI application using Uvicorn server
-    uvicorn.run("app:app", host="localhost", port=8000, reload=True)
